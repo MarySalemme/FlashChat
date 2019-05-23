930a1e9fa48d6778e73d725ee1ffe1073777f92c
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-05-23 10:53:16 +0100

    Implement signout

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Firebase
 
 class ChatViewController: UIViewController {
     
@@ -106,9 +106,13 @@ class ChatViewController: UIViewController {
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
-        //TODO: Log out the user and send them back to WelcomeViewController
-        
-        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        }
+        catch {
+            print("Error: issue with signout")
+        }
     }
     
 
