391e2a1e233dd057acdbe2c5eaff517773748fb5
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-05-24 10:32:26 +0100

    Retrieve messages from DB

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-    // Declare instance variables here
 
+    var messageArray: [Message] = [Message]()
     
     // We've pre-linked the IBOutlets
     @IBOutlet var heightConstraint: NSLayoutConstraint!
@@ -32,6 +32,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         configureTableView()
+        retrieveMessages()
     }
 
     ///////////////////////////////////////////
@@ -45,16 +46,16 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         /// indexPath is the location of the cell we're initialising
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["First Message", "Second Message", "Third Message"]
-        
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
         return cell
     }
     
     // Specifies how many cells I want
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     @objc func tableViewTapped() {
@@ -90,11 +91,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     
     //MARK: - Send & Recieve from Firebase
-    
-    
-    
-    
-    
+
     @IBAction func sendPressed(_ sender: AnyObject) {
         messageTextfield.endEditing(true)
         messageTextfield.isEnabled = false
@@ -119,11 +116,24 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     //TODO: Create the retrieveMessages method here:
-    
-    
-
-    
-    
+    func retrieveMessages() {
+        let messagesDB = Database.database().reference().child("Messages")
+        
+        messagesDB.observe(.childAdded, with: { (snapshot) in
+            let snapshotValue = snapshot.value as! Dictionary<String, String>
+            
+            let text = snapshotValue["MessageBody"]!
+            let sender = snapshotValue["Sender"]!
+            
+            let message = Message()
+            message.messageBody = text
+            message.sender = sender
+            
+            self.messageArray.append(message)
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        })
+    }
     
     @IBAction func logOutPressed(_ sender: AnyObject) {
         
