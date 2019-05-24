f61bbcbd3d27a7084fdd8c4b26598a280e905f2f
Mary Salemme <mary.salemme@bglgroup.co.uk>
2019-05-24 10:55:07 +0100

    Add loading spinner on login and register screens

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -16,20 +17,22 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-   
     @IBAction func logInPressed(_ sender: AnyObject) {
+        
+        SVProgressHUD.show()
+        
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
             if error != nil {
                 print("Error: \(error!)")
             } else {
                 print("Login successful")
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
